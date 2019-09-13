211335b9ddfda6b8976029bbe0bd6ae7666276a4
Krishna Ajmeri <krishnaajmeri93@gmail.com>
2019-09-13 11:35:57 -0400

    Initial Commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Krishna Ajmeri on 9/13/19.
+//  Copyright © 2019 Krishna Ajmeri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
